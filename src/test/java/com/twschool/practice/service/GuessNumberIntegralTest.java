d79d54ba5b25a9297a83841e7e14e4f7cb35af0e
huyuanixn325 <huyuanxin0325@163.com>
2020-06-17 21:17:19 +0800

    finished should_return_0_when_given_1235_1237

--- src/test/java/com/twschool/practice/service/GuessNumberIntegralTest.java
+++ src/test/java/com/twschool/practice/service/GuessNumberIntegralTest.java
@@ -58,4 +58,23 @@ public class GuessNumberIntegralTest {
         //then
         Assert.assertEquals(0,result);
     }
+
+    @Test
+    public void should_return_0_when_given_1235_1237() {
+        //given
+        String currentAnswer = "1 2 3 5";
+        String answer1 = "1 2 3 7";
+
+        Answer answer = new Answer(Arrays.asList("1", "2", "3", "5"));
+        RandomAnswerGenerator randomAnswerGenerator = Mockito.mock(RandomAnswerGenerator.class);
+        Mockito.when(randomAnswerGenerator.generateAnswer()).thenReturn(answer);
+        GuessNumberGame guessNumberGame = new GuessNumberGame(randomAnswerGenerator);
+        GameService gameService = new GameService(guessNumberGame);
+        //when
+        gameService.integral(currentAnswer);
+        gameService.integral(answer1);
+        int result = gameService.integral(answer1);
+        //then
+        Assert.assertEquals(0,result);
+    }
 }
